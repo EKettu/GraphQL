d8b9b29686c4d2a6406e93203d62dd1880833d2d
EKettu <elina.kettunen@helsinki.fi>
2020-11-27 11:48:17 +0200

    more queries

--- index.js
+++ index.js
@@ -79,17 +79,43 @@ let books = [
 ]
 
 const typeDefs = gql`
+ type Book {
+    title: String!
+    published: Int
+    author: String!
+    id: ID!
+    genres: [String!]  
+  }
+  type Author {
+      name: String!
+      id: ID!
+      bookCount: Int
+  }
   type Query {
       bookCount: Int!
       authorCount: Int!
+      allBooks(author: String!): [Book!]!
+      allAuthors: [Author!]!
   }
 `
 
 const resolvers = {
     Query: {
         bookCount: () => books.length,
-        authorCount: () => authors.length
-      }
+        authorCount: () => authors.length,
+        allBooks: (root, args) => {
+            if(!args.author) {
+                return books
+            }
+            return books.filter((book) => book.author === args.author)
+        },
+        allAuthors: () => authors,
+    },
+    Author: {
+       bookCount : (root) => {
+            return books.filter((book) => book.author === root.name).length
+       }     
+    }
 }
 
 const server = new ApolloServer({
