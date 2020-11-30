b9fe1fce9404cd393b37c7dfe950411db7cf158c
EKettu <elina.kettunen@helsinki.fi>
2020-11-30 15:09:58 +0200

    frontend with authors

---
+++ frontend/src/components/Books.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Books = (props) => {
+  if (!props.show) {
+    return null
+  }
+  console.log(props)
+
+  const books = props.data
+
+  return (
+    <div>
+      <h2>books</h2>
+
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              author
+            </th>
+            <th>
+              published
+            </th>
+          </tr>
+          {books.map(book =>
+            <tr key={book.title}>
+              <td>{book.title}</td>
+              <td>{book.author}</td>
+              <td>{book.published}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Books
